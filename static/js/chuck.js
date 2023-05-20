0953377d69b44b553e9d04a17204a30ccaf8b494
o-crii <cristianpesola@gmail.com>
2023-05-20 10:40:32 +0200

    finendo javascript

--- static/js/chuck.js
+++ static/js/chuck.js
@@ -1,92 +1,42 @@
-const loadingJoke = document.querySelector("#loadingJoke")
-const copyJoke = document.querySelector("#CopyJoke")
-const jokeTxt = document.querySelector("#joke")
+document.querySelector('#loadingJoke').addEventListener('submit', function(e) {
+	e.preventDefalut()
 
-loadingJoke.addEventListener('click', function(e){
-	e.preventDefault()
-	loadingJokeTxt()
+	let categoria = document.querySelector('.select[name=select]').value
+
+	if (categoria==='Random') {
+		fetch("https://api.chucknorris.io/jokes/random")
+		.then(thenCallback)
+		.then(finalCallback)
+		.catch(catchCallback)
+	} else {
+		fetch(`https://api.chucknorris.io/jokes/random?category=${categoria}`)
+
+		.then(thenCallback)
+		.then(finalCallback)
+		.catch(catchCallback)
+	}
+
+
 })
 
-function loadCategories(){
-    let categories = ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"]
-    //let category = categorySelect.value
+function thenCallback(response){
 
-    categories.forEach(category => {
-        const option = document.createElement("option")
-        option.value = category
-        option.innerHTML = category
-        categorySelect.appendChild(option)
-    })
-
-}
-
-function loadingJokeTxt(){
-	let url = 'https://api.chucknorris.io/jokes/random'
-	let category = categorySelect.value
-
-	if(category === "Casuale"){
-        fetch("https://api.chucknorris.io/jokes/random")
-
-            .then(ThenCallActive)
-
-            .then(FinalCallBack)
-
-            .catch(CatchCallActive)
-
-    //prende una category random https://....../random    
-    } else {
-        fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
-
-            .then(ThenCallActive)
-
-            .then(FinalCallBack)
-
-            .catch(CatchCallActive)
-
-    //prende la categoria da noi selezionata 
-    }
-}
-
-function CatchCallActive(){
-    document.querySelector(".ChuckNorrisPhraseContent").innerHTML = "Riprova!"
-}
-
-function ThenCallActive(response){
     if(response.status === 200){
         return response.json()
     }
 }
 
-function FinalCallBack(data){
-    document.querySelector(".ChuckNorrisPhraseContent").innerHTML = data.value
-    const urlContent = document.querySelector(".ChuckNorrisUrlsContent").innerHTML = data.url
+function finalCallback(data) {
+    console.log(data)
 
-    document.querySelector("#ChuckNorrisLink").addEventListener('click', function(){
-        window.location.href = urlContent
-    })
+    
+    document.querySelector("#joke").innerHTML = data.value
+
+    /*document.querySelector(".urlBox").setAttribute('href', data.url)
+    document.querySelector(".urlBox").classList.remove("disabled")*/    
 }
 
-document.querySelector(".ChuckNorrisButtonCopied").addEventListener("click", function(e) {
-    let CopyArea = document.querySelector(".ChuckNorrisPhraseContent");
-
-    let range = document.createRange();
-    range.selectNode(CopyArea);
-
-    navigator.clipboard.writeText(CopyArea.textContent);
-    let CopyAler = document.execCommand('copy');
-    alert('testo copiato: '+ CopyArea.textContent);
-    return CopyAler;
-})
-
-loadCategories()
-
-
-
-
-
-
-
-
-
-
-
+function catchCallback(error) {
+    error = "Errore104!"
+    document.querySelector("#joke").innerHTML = error
+}
